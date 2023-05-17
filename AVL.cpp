2542d64c0e92c6a23407c02bbada9b9b8b784450
Ангальдт Михаил <beat1097@mail.ru>
2023-05-17 19:00:31 +0700

    -

--- AVL.cpp
+++ AVL.cpp
@@ -21,17 +21,18 @@ struct BST
     node* add(int x, node* t)
     {   //функция добавления узла в дерево
         if(t == NULL)
-        {   // если нет корня
+        {   //если нет корня
             t = new node;
             t->data = x;
             t->height = 0;
             t->left = t->right = NULL;
         }
         else if(x < t->data)
-        {   
+        {   //если дерево не пустое и ключ меньше ключа
+            //текущего узла
             t->left = add(x, t->left);
             if(height(t->left) - height(t->right) == 2)
-            {
+            {   //если разница высот потомков 2 делаем балансировку
                 if(x < t->left->data)
                     t = singleRightRotate(t);
                 else
@@ -39,23 +40,24 @@ struct BST
             }
         }
         else if(x > t->data)
-        {
+        {   //если дерево не пустое и ключ больше ключа
+            //текущего узла
             t->right = add(x, t->right);
             if(height(t->right) - height(t->left) == 2)
-            {
+            {   //если разница высот потомков 2 делаем балансировку
                 if(x > t->right->data)
                     t = singleLeftRotate(t);
                 else
                     t = doubleLeftRotate(t);
             }
         }
-
+        //определяем высоту добавленного узла
         t->height = max(height(t->left), height(t->right))+1;
         return t;
     }
 
     node* singleRightRotate(node* &t)
-    {
+    {   //правый поворот
         node* u = t->left;
         t->left = u->right;
         u->right = t;
@@ -65,7 +67,7 @@ struct BST
     }
 
     node* singleLeftRotate(node* &t)
-    {
+    {   //левый поворот
         node* u = t->right;
         t->right = u->left;
         u->left = t;
@@ -75,24 +77,24 @@ struct BST
     }
 
     node* doubleLeftRotate(node* &t)
-    {
+    {   //двойной левый поворот
         t->right = singleRightRotate(t->right);
         return singleLeftRotate(t);
     }
 
     node* doubleRightRotate(node* &t)
-    {
+    {   //двойной правый поворот
         t->left = singleLeftRotate(t->left);
         return singleRightRotate(t);
     }
 
     int height(node* t)
-    {
+    {   //возвращает высоту узла
         return (t == NULL ? -1 : t->height);
     }
 
     int getBalance(node* t)
-    {
+    {   //возвращает разницу высот потомков
         if(t == NULL)
             return 0;
         else
@@ -100,7 +102,7 @@ struct BST
     }
 
     void add(int x)
-    {
+    {   //добавляет узел в дерево
         root = add(x, root);
     }
 
@@ -115,12 +117,14 @@ struct BST
         }
     }
     bool IsStrictlyBinary(node* root) { // Проверить является ли дерево строгим
-
+        //если нет потомков считаем узел строгим
         if (root->left == NULL && root->right == NULL)
             return true;
+        //если есть один потом считаем узел нестрогим
         else if ((root->left == NULL && root->right != NULL) ||
             (root->right == NULL && root->left != NULL))
             return false;
+        //если и левый и правый узлы не имеют потомков считаем дерево строгим
         else
             return IsStrictlyBinary(root->left) && IsStrictlyBinary(root->right);
     }
@@ -145,17 +149,8 @@ int main()
         cin >> x;
         t.add(x);
     }
-    t.print_Tree(t.root, 0);
+    t.print_Tree(t.root, 2);
     if(t.IsStrictlyBinary(t.root))cout << "Дерево является строгим";
     else cout << "Дерево не является строгим";
     
-    /*t.remove(5);
-    t.remove(35);
-    t.remove(65);
-    t.remove(89);
-    t.remove(43);
-    t.remove(88);
-    t.remove(20);
-    t.remove(38);*/
-    //t.display();
 }
